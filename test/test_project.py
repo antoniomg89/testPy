27447b8d7119961b7302b4c6b13437c1ac00ada9
Antonio <cuentadisponible89@gmail.com>
2022-11-20 19:31:17 +0100

    Update requirements

--- test/test_project.py
+++ test/test_project.py
@@ -1,24 +1,3 @@
-#import unittest
-
-#from src.project import project
-
-#class TestProject(unittest.TestCase):
-    #def setUp(self):
-     #   self.obj_tester = project()
-
-    #def test_object_OK(self):
-     #   self.assertIsInstance(self.obj_tester,project, "Object OK")
-
-    #def test_getParam1_correctly(self):
-   #     self.assertIsInstance(self.obj_tester.getParam1(),int, "Param1 OK")
-
-  #  def test_getParam2_correctly(self):
- #       self.assertIsInstance(self.obj_tester.getParam2(),int, "Param2 OK")
-
-#if __name__ == '__main__':
-#    unittest.main()
-import pytest
-
 from src.project import project
 
 class TestProject:
